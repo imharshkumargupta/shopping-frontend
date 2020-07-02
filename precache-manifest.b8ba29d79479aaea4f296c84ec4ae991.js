4c1e2c364abd395b97f135195a2f7efbdbbdb94c
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2020-07-02 09:02:40 +0530

    Updates

---
+++ precache-manifest.b8ba29d79479aaea4f296c84ec4ae991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6d6ea0ba926208ae5e3",
+    "url": "/shopping-frontend/static/css/main.47dfef39.chunk.css"
+  },
+  {
+    "revision": "f6d6ea0ba926208ae5e3",
+    "url": "/shopping-frontend/static/js/main.f6d6ea0b.chunk.js"
+  },
+  {
+    "revision": "02684ddd7f2befb9d634",
+    "url": "/shopping-frontend/static/js/1.02684ddd.chunk.js"
+  },
+  {
+    "revision": "028614912196a730abc4",
+    "url": "/shopping-frontend/static/js/runtime~main.02861491.js"
+  },
+  {
+    "revision": "d529f69eac213a52560fd9cd41266ecc",
+    "url": "/shopping-frontend/index.html"
+  }
+];
